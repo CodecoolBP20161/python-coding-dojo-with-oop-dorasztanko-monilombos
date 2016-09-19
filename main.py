c3229f8d125cbc6ff61979a583b10060e9707e79
dsztanko <szt.dori@gmail.com>
2016-09-19 15:13:34 +0200

    step 5 completed

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-
-
 # Step 1
-class Contact:
+class Contact(object):
     # Step 2
     all_contacts = []
 
@@ -10,10 +8,22 @@ class Contact:
         self.email = email
         Contact.all_contacts.append(self)
 
+    # def __str__(self):
+    #     return self.name
+    #
+    # def __repr__(self):
+    #     return self.name
+
+
 # Step 3
     @classmethod
     def reset_contacts(cls):
-        cls.all_contacts = []
+        if cls.all_contacts is None:
+            pass
+        else:
+            while cls.all_contacts:
+                for element in cls.all_contacts:
+                    cls.all_contacts.remove(element)
 
 
 # Step 4
@@ -21,18 +31,32 @@ class Supplier(Contact):
     # Step 5
     all_orders = {}
 
-    def __inti__(self, item):
-        super(Contact, self).__init__(name, email)
-        self.item = item
+    def order(self, item):
+        # for value in Supplier.all_orders.values():
+        #     print(value)
+        if self.name in Supplier.all_orders.keys():
+            Supplier.all_orders[self.name].append(item)
+        else:
+            Supplier.all_orders.update({self.name: [item]})
 
-    # @classmethod
-    # def order(cls):
 
-
-
-# contact = Contact('Moni', 'moni@net.hu')
+supplier2 = Supplier('Moni', 'moni@net.hu')
+# print(contact)
 # contact2 = Contact('Dori', 'dori@net.hu')
 # print(contact.name)
+# print(contact.email)
 # print(Contact.all_contacts)
 # Contact.reset_contacts()
 # print(Contact.all_contacts)
+supplier = Supplier('Anna', 'anna@net.com')
+# print(supplier.name)
+supplier.order('alma')
+supplier2.order('kréta')
+
+supplier.order('körte')
+# supplier.order('kréta')
+print(Supplier.all_orders)
+
+
+
+
